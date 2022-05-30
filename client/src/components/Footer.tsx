58cdcf567e531e07e4d506005f91ab86abc7096d
Changwoo Lee <lcwoo31456@gmail.com>
2022-05-30 13:08:05 +0900

    Login Form 디자인 작업

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,35 +8,35 @@ const Footer = () => {
         <Link to="/" className="hover:text-neutral-500">
           Legal
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Privacy
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Cookie Policy
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Consent Manager
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Imprint
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Creator Resources
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Blog
         </Link>
-        ∙
+        <span className="px-1">∙</span>
         <Link to="/" className="hover:text-neutral-500">
           Charts
         </Link>
-        ∙
+        <span className="px-1">∙</span>
       </div>
       <div className="pt-2 cursor-pointer group text-xs">
         <span className="text-blue-600 mr-1 group-hover:text-neutral-700">
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 import Footer from "../components/Footer";
 
 function SignIn() {
@@ -11,7 +12,32 @@ function SignIn() {
             albums
           </h2>
         </div>
-        <div className="w-full h-auto border p-8">Login Component</div>
+        <div className="w-full h-auto border p-8">
+          <div className="flex flex-col py-8">
+            {/*SNS Login*/}
+            <div className="w-full">
+              <div className="w-full py-2 flex justify-center items-center border border-gray-200 rounded-sm cursor-pointer">
+                <FcGoogle className="mr-2" />
+                Continue with Google
+              </div>
+            </div>
+            <div className="flex items-center justify-between py-4 w-full">
+              <hr className="w-full border-neutral-900" />
+              <p className="px-4">or</p>
+              <hr className="w-full border-neutral-900" />
+            </div>
+            <form className="flex flex-col text-lg">
+              <input
+                type="text"
+                placeholder="Your email address"
+                className="border rounded-sm px-4 py-1.5 focus:outline-none"
+              />
+              <button className="w-full py-1.5 bg-orange-600 mt-4 rounded-sm text-white">
+                Continue
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
