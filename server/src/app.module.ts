a444f068baae9e8baf0a70bc631720989a85aeed
Changwoo Lee <lcwoo31456@gmail.com>
2022-06-01 20:55:09 +0900

    Server - DB 연결(postgresql)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'woo3145',
+      database: 'sound-cloud-dev',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
