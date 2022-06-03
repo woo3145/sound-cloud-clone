79b8a0a03a577c982060579c40c10cd12239ccc5
Changwoo Lee <lcwoo31456@gmail.com>
2022-06-04 04:34:41 +0900

    ConfigModule - ENV 설정, Joi 환경변 검증

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,15 +4,31 @@ import { AppController } from './app.controller';
 import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().default(4000),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+      envFilePath: '.env.dev',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'woo3145',
-      database: 'sound-cloud-dev',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_NAME,
       entities: [User],
       synchronize: true,
     }),
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -6,8 +6,8 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(@Request() req) {
+  @Post('signin')
+  async signin(@Request() req) {
     return this.authService.login(req.user);
   }
 }
